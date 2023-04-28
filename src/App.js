0686381998b3cadbb39393ce52b216a6298a90cf
Ayush Bohra <codefingers809@gmail.com>
2023-04-28 06:34:25 +0530

    lines connecting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import { useEffect, useRef } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useRef, useState } from "react";
+
+// TODO: piyush wala part yaad karna hai
+// sensors mein phd
+// code complete karna hai
 
 function App() {
   const canvasRef = useRef(null);
   const balls = [];
   const ballRadius = 10;
-  const numBalls = 20;
-  const numObj = 5;
+  const [numBalls, setNumBalls] = useState(20);
+  const [numObj, setNumObj] = useState(5);
+  const [lineDisplay, setLineDisplay] = useState(false);
 
   useEffect(() => {
+    //canvas
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-    // Set the canvas width and height to match the parent wrapper
     canvas.width = canvas.parentElement.clientWidth;
     canvas.height = canvas.parentElement.clientHeight;
 
-    // Create the balls with random positions
+    //add balls
     for (let i = 0; i < numBalls; i++) {
       balls.push({
         x: Math.random() * (canvas.width - 2 * ballRadius) + ballRadius,
         y: Math.random() * (canvas.height - 2 * ballRadius) + ballRadius,
-        dx: (Math.random() - 0.5) * 4,
-        dy: (Math.random() - 0.5) * 4,
+        angle: Math.random() * 2 * Math.PI,
+        prevVal: 0,
         type: "person",
       });
     }
@@ -31,14 +37,19 @@ function App() {
         type: "object",
       });
     }
+    for (let i = 0; i < balls.length; i++) {
+      if (balls[i].type === "person") {
+        balls[i].dx = 2 * Math.cos(balls[i].angle);
+        balls[i].dy = 2 * Math.sin(balls[i].angle);
+      }
+    }
 
-    // Animate the balls
     function animate() {
       // Clear the canvas
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       // Loop through the balls
-      for (let i = 0; i < numBalls; i++) {
+      for (let i = 0; i < balls.length; i++) {
         if (balls[i].type === "object") {
           // Draw the ball
           ctx.beginPath();
@@ -46,47 +57,111 @@ function App() {
           ctx.fillStyle = "gray";
           ctx.fill();
           ctx.closePath();
-          continue;
-        }
-        // Update the ball position
-        balls[i].x += balls[i].dx;
-        balls[i].y += balls[i].dy;
+        } else {
+          // Update the ball position
+          balls[i].x += balls[i].dx;
+          balls[i].y += balls[i].dy;
 
-        // Bounce the ball off the edges of the canvas
-        if (
-          balls[i].x + ballRadius > canvas.width ||
-          balls[i].x - ballRadius < 0
-        ) {
-          balls[i].dx = -balls[i].dx;
-        }
-        if (
-          balls[i].y + ballRadius > canvas.height ||
-          balls[i].y - ballRadius < 0
-        ) {
-          balls[i].dy = -balls[i].dy;
-        }
+          // Bounce the ball off the edges of the canvas
+          if (
+            balls[i].x + ballRadius > canvas.width ||
+            balls[i].x - ballRadius < 0
+          ) {
+            balls[i].dx = -balls[i].dx;
+          }
+          if (
+            balls[i].y + ballRadius > canvas.height ||
+            balls[i].y - ballRadius < 0
+          ) {
+            balls[i].dy = -balls[i].dy;
+          }
 
-        // Draw the ball
-        ctx.beginPath();
-        ctx.arc(balls[i].x, balls[i].y, ballRadius, 0, Math.PI * 2);
-        ctx.fillStyle = "blue";
-        ctx.fill();
-        ctx.closePath();
+          // Draw the ball
+          ctx.beginPath();
+          ctx.arc(balls[i].x, balls[i].y, ballRadius, 0, Math.PI * 2);
+          ctx.fillStyle = "blue";
+          ctx.fill();
+          ctx.closePath();
+        }
+        if (lineDisplay) {
+          // draw lines sensor1
+          ctx.beginPath();
+          ctx.moveTo(50, canvas.height - 50);
+          ctx.lineTo(balls[i].x, balls[i].y);
+          ctx.strokeStyle = balls[i].type === "person" ? "lime" : "gray";
+          ctx.stroke();
+          // draw lines sensor2
+          ctx.beginPath();
+          ctx.moveTo(canvas.width - 50, canvas.height - 50);
+          ctx.lineTo(balls[i].x, balls[i].y);
+          ctx.stroke();
+          // draw lines height
+          if (balls[i].type === "person") {
+            ctx.beginPath();
+            ctx.moveTo(balls[i].x, canvas.height - 50);
+            ctx.lineTo(balls[i].x, balls[i].y);
+            ctx.strokeStyle = "brown";
+            ctx.stroke();
+          }
+        }
       }
+
+      if (lineDisplay) {
+        // draw line joining sensor
+        ctx.beginPath();
+        ctx.moveTo(50, canvas.height - 50);
+        ctx.lineTo(canvas.width - 50, canvas.height - 50);
+        ctx.stroke();
+      }
+      // Draw circle in bottom left corner
+      ctx.beginPath();
+      ctx.arc(50, canvas.height - 50, 25, 0, 2 * Math.PI);
+      ctx.fillStyle = "red";
+      ctx.fill();
+
+      // Draw circle in bottom right corner
+      ctx.beginPath();
+      ctx.arc(canvas.width - 50, canvas.height - 50, 25, 0, 2 * Math.PI);
+      ctx.fillStyle = "red";
+      ctx.fill();
 
       // Request the next frame of animation
       requestAnimationFrame(animate);
     }
-
     // Start the animation
     animate();
-  }, []);
+  }, [lineDisplay, numBalls, numObj]);
   return (
     <div className="App flex justify-center items-center flex-col h-screen w-screen p-4">
-      <div className="controlBox">{/* Density: {density} */}</div>
+      <div className="controlBox mb-4 flex flex-row items-center justify-center space-x-4">
+        <p className="text-xl">
+          Density:&nbsp;
+          <button
+            className="p-4 bg-blue-600 rounded-xl text-white hover:bg-blue-800"
+            onClick={() => setNumBalls((c) => (c > 0 ? --c : c))}
+          >
+            -
+          </button>
+          {numBalls}
+          <button
+            className="p-4 bg-blue-600 rounded-xl text-white hover:bg-blue-800"
+            onClick={() => setNumBalls(numBalls + 1)}
+          >
+            +
+          </button>
+        </p>
+        {/* display lines */}
+        <button
+          className="p-4 bg-blue-600 rounded-xl text-white hover:bg-blue-800"
+          onClick={() => setLineDisplay(!lineDisplay)}
+        >
+          Show Lines
+        </button>
+      </div>
+
       <div className="canvasWrap w-full flex-1">
         <canvas
-          className="walkBox border-2 border-black rounded-xl"
+          className="walkBox shadow-[0_0_0_2px_black] rounded-xl"
           ref={canvasRef}
         ></canvas>
       </div>
@@ -99,4 +174,3 @@ function App() {
 }
 
 export default App;
-
